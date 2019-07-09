48a44be91922ee3e03feab87c2a962f3c28fcb91
alnyli07 <altgrlpinar@gmail.com>
2019-07-09 11:37:05 +0000

    Add temp profile

--- scripts/pages/pgIslemlerim.js
+++ scripts/pages/pgIslemlerim.js
@@ -1,16 +1,17 @@
 const extend = require("js-base/core/extend");
 const Page2Design = require('ui/ui_pgIslemlerim');
 
+const profileGravatar = "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?r=pg"
 const Page2 = extend(Page2Design)(
-    // Constructor
-    function(_super, routeData, router) {
-        // Initalizes super class for this page scope
-        _super(this);
-        // Overrides super.onShow method
-        this.onShow = onShow.bind(this, this.onShow.bind(this));
-        // Overrides super.onLoad method
-        this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
-    });
+	// Constructor
+	function(_super, routeData, router) {
+		// Initalizes super class for this page scope
+		_super(this);
+		// Overrides super.onShow method
+		this.onShow = onShow.bind(this, this.onShow.bind(this));
+		// Overrides super.onLoad method
+		this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
+	});
 
 /**
  * @event onShow
@@ -19,8 +20,8 @@ const Page2 = extend(Page2Design)(
  * @param {Object} parameters passed from Router.go function
  */
 function onShow(superOnShow) {
-    superOnShow();
-    console.log("pgIslemlerim");
+	superOnShow();
+	console.log("pgIslemlerim");
 }
 
 /**
@@ -29,8 +30,14 @@ function onShow(superOnShow) {
  * @param {function} superOnLoad super onLoad function
  */
 function onLoad(superOnLoad) {
-    superOnLoad();
-    this.headerBar.leftItemEnabled = false;
+	superOnLoad();
+	this.headerBar.leftItemEnabled = false;
+	this.flUserHeaderBar.imgUser.loadFromUrl({
+		url: profileGravatar,
+		onSuccess: () => {
+			this.flUserHeaderBar.imgUser.image.android.round(12);
+		}
+	});
 }
 
 module.exports = Page2;
