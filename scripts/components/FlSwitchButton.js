8d4bd767f26661f5683a5f03e2c631f4c3d78a2f
alnyli07 <altgrlpinar@gmail.com>
2019-07-10 08:19:34 +0000

    Add switchbutton functionality

--- scripts/components/FlSwitchButton.js
+++ scripts/components/FlSwitchButton.js
@@ -1,17 +1,58 @@
 const extend = require('js-base/core/extend');
 const FlSwitchButtonDesign = require('library/FlSwitchButton');
+const touch = require("sf-extension-utils/lib/touch");
 
+const ACTIVE_CLASS_NAME = ".flSwitchButton-button.active"
 const FlSwitchButton = extend(FlSwitchButtonDesign)(
 	// Constructor
 	function(_super, props = {}, pageName) {
 		// Initalizes super class for this scope
 		_super(this, props);
 		this.pageName = pageName;
+		this._data = [];
+		this._currentActiveIndex = 0;
 		this.setData = data => {
+			this._data = data.slice();
 			const childList = this.getChildList();
-			childList.forEach((child, index) => child.text = data[index].text);
+			childList.forEach((child, index) => {
+				touch.addPressEvent(child, () => {
+					if (!this._data[index].isActive) {
+						this.onIndexChange && this.onIndexChange(index);
+						this._data[index].isActive = true;
+						this._data[this._currentActiveIndex].isActive = false;
+						updateItems(this);
+					}
+				});
+			});
+			updateItems(this);
 		};
+
 	}
 );
+
+function updateItems(comp) {
+	const childList = comp.getChildList();
+	childList.forEach((child, index) => {
+		child.text = comp._data[index].text;
+		comp._data[index].isActive && (comp._currentActiveIndex = index);
+		updateLayout(child, comp._data[index].isActive);
+	});
+}
+
+function updateLayout(comp, isActive) {
+	if (isActive) {
+		comp.dispatch({
+			type: "pushClassNames",
+			classNames: [ACTIVE_CLASS_NAME]
+		});
+	}
+	else {
+		comp.dispatch({
+			type: "removeClassName",
+			className: [ACTIVE_CLASS_NAME]
+		});
+
+	}
+}
 
 module.exports = FlSwitchButton;
