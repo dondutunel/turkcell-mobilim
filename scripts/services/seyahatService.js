ef20417c0f9a608f703b8b47d482c557a8176cc5
alnyli07 <altgrlpinar@gmail.com>
2019-07-11 08:33:02 +0000

    Add services

---
+++ scripts/services/seyahatService.js
@@ -0,0 +1,155 @@
+module.exports = {
+    getAccomodationOptions: () => new Promise((resolve, reject) => {
+        setTimeout(() => resolve({
+            "roomTypes": [{
+                "Code": "D",
+                "Description": "Double"
+            }],
+            "accomodationTypes": [{
+                "Code": "B",
+                "Description": "Bed"
+            }],
+            "hotelReasons": [{
+                "Code": 1,
+                "Description": "Anlaşmalı otellerde yer olmaması"
+            }]
+        }), 300);
+    }),
+    getAgentList: () => new Promise((resolve, reject) => {
+        setTimeout(() => resolve(
+            [{
+                "AdCode": "TCELL|TEAM-ACENTE-DOK-TUR",
+                "AgentID": 201,
+                "AgentName": "DOK-TUR",
+                "UserGroup": "ACENTA_DOK-TUR"
+            }, {
+                "AdCode": "TCELL|TEAM-ACENTE-VISTA",
+                "AgentID": 61,
+                "AgentName": "VISTA",
+                "UserGroup": "ACENTA_VISTA"
+            }]
+        ), 300);
+    }),
+    getAirportOptions: () => new Promise((resolve, reject) => {
+        setTimeout(() => resolve({
+            "from": [{
+                "AirPortName_Turkish": "Kars Havalimanı",
+                "AirportName_English": "Kars Havalimanı",
+                "City": "Kars",
+                "CityID": 1279,
+                "Country": "TÜRKİYE",
+                "CountryID": 190,
+                "DefaultAirport": 0,
+                "ID": 26
+            }, {
+                "AirPortName_Turkish": "Oskarshamn Havaalanı",
+                "AirportName_English": "Oskarshamn Havaalani",
+                "City": "Oskarshamn",
+                "CityID": 2104,
+                "Country": "İSVEÇ",
+                "CountryID": 83,
+                "DefaultAirport": 0,
+                "ID": 479
+            }],
+            "to": [{
+                "AirPortName_Turkish": "Sıvas Havalimanı",
+                "AirportName_English": "Sıvas Havalimanı",
+                "City": "Sivas",
+                "CityID": 2634,
+                "Country": "TÜRKİYE",
+                "CountryID": 190,
+                "DefaultAirport": 0,
+                "ID": 39
+            }]
+        }), 300);
+    }),
+    getAutocompleteCity: () => new Promise((resolve, reject) => {
+        setTimeout(() => resolve(
+            [{
+                "AirPortName_Turkish": "",
+                "AirportName_English": "",
+                "City": "Kars",
+                "CityID": 1279,
+                "Country": "TÜRKİYE",
+                "CountryID": 190,
+                "DefaultAirport": 0,
+                "ID": 26
+            }, {
+                "AirPortName_Turkish": "",
+                "AirportName_English": "",
+                "City": "Oskarshamn",
+                "CityID": 2104,
+                "Country": "İSVEÇ",
+                "CountryID": 83,
+                "DefaultAirport": 0,
+                "ID": 479
+            }]
+        ), 300);
+    }),
+    getTripConditions: () => new Promise((resolve, reject) => {
+        setTimeout(() => resolve({
+            "Code": 17,
+            "Description": "<div/>"
+        }), 300);
+    }),
+    getTripOptions: () => new Promise((resolve, reject) => {
+        setTimeout(() => resolve({
+            "types": [{
+                "Code": "YURTICI",
+                "Description": "Yurt İçi"
+            }],
+            "purposes": [{
+                "Code": "SEMINER",
+                "Description": "Seminer - Konferans"
+            }]
+        }), 300);
+    }),
+    getHotels: () => new Promise((resolve, reject) => {
+        setTimeout(() => resolve([{
+            "District": "ÇANKAYA",
+            "OtelId": 2,
+            "OtelName": "Ankara Hiltonsa"
+        }, {
+            "District": "SÖĞÜTÖZÜ",
+            "OtelId": 20,
+            "OtelName": "Wyndham Ankara"
+        }, {
+            "District": "ÇANKAYA",
+            "OtelId": 22,
+            "OtelName": "Swissotel Ankara"
+        }, {
+            "District": "YENİMAHALLE",
+            "OtelId": 107,
+            "OtelName": "Movenpick Hotel Ankara"
+        }, {
+            "District": "GİMAT",
+            "OtelId": 347,
+            "OtelName": "Hilton Garden Inn Ankara Gimat (Ankara)"
+        }, {
+            "District": "ÇANKAYA",
+            "OtelId": 348,
+            "OtelName": "Point Hotel Ankara"
+        }, {
+            "District": "Ostim",
+            "OtelId": 406,
+            "OtelName": "Ostimpark Business Hotel (Ankara)"
+        }, {
+            "District": "Yenimahalle",
+            "OtelId": 434,
+            "OtelName": "Crowne Plaza"
+        }, {
+            "District": "Ulus",
+            "OtelId": 437,
+            "OtelName": "Radisson Blu Ankara"
+        }, {
+            "District": "",
+            "OtelId": "DIGER",
+            "OtelName": "Diğer"
+        }]), 300);
+    }),
+    getHotelPrice: () => new Promise((resolve, reject) => {
+        setTimeout(() => resolve({
+            "price": "666,72TL"
+        }), 300);
+    })
+};
