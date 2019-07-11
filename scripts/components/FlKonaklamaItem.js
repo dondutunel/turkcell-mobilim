bde5e002e7dbdb5045f00c916f18f1bd4d38403f
ozcanovunc <ozcanovunc@outlook.com>
2019-07-11 08:35:03 +0000

    In progress

--- scripts/components/FlKonaklamaItem.js
+++ scripts/components/FlKonaklamaItem.js
@@ -1,34 +1,43 @@
 const extend = require('js-base/core/extend');
 const FlKonaklamaItemDesign = require('library/FlKonaklamaItem');
 const options = {
-	"mtLocationFrom": {
-		hint: "Nereden"
-	},
-	"mtLocationTo": {
-		hint: "Nereye"
-	},
-	"mtDateFrom": {
-		hint: "Tarih"
-	},
-	"mtTimeFrom": {
-		hint: "Saat"
-	},
+    "mtLocationFrom": {
+        hint: "Nereden"
+    },
+    "mtLocationTo": {
+        hint: "Nereye"
+    },
+    "mtDateFrom": {
+        hint: "Tarih"
+    },
+    "mtTimeFrom": {
+        hint: "Saat"
+    },
+    "mtDonusDate": {
+        hint: "Tarih"
+    },
+    "mtDonusTime": {
+        hint: "Saat"
+    },
 };
 
 const FlKonaklamaItem = extend(FlKonaklamaItemDesign)(
-	// Constructor
-	function(_super, props = {}, pageName) {
-		// Initalizes super class for this scope
-		_super(this, props);
-		this.pageName = pageName;
+    // Constructor
+    function(_super, props = {}, pageName) {
+        // Initalizes super class for this scope
+        _super(this, props);
+        this.pageName = pageName;
 
-		this.init = () => {
-			Object.keys(options).forEach(componentName => {
-				this[componentName].options = options[componentName];
-			});
-			this.flDonusTarihi.setData({ text: "Dönüş Tarihi" });
-		};
-	}
+        this.init = () => {
+            Object.keys(options).forEach(componentName => {
+                this[componentName].options = options[componentName];
+            });
+            this.flCheckbox.setData({ text: "Dönüş Tarihi" });
+            this.flCheckbox.onCheckedChange = (isChecked) => {
+
+            };
+        };
+    }
 );
 
 module.exports = FlKonaklamaItem;
